321b3389efc2f9ab5fbed5ff8b2f947a0978306b
Phuong <phuong.tran@jvncomm.com>
2020-02-07 11:00:09 -0500

    Test async client.

--- Test_MQTT_Raspberry_Pi/main.cpp
+++ Test_MQTT_Raspberry_Pi/main.cpp
@@ -1,34 +1,74 @@
 #include "mqtt/async_client.h"
 #include "fmt/format.h"
 
+class MQTT_Subscriber : public virtual mqtt::callback,
+	public virtual mqtt::iaction_listener
+{
+private:
+	std::string address_;
+	std::string topic_;
+	int qos_;
+	mqtt::async_client client_;
+	mqtt::connect_options connOpts;
+
+private:
+	void on_failure(const mqtt::token& tok) override {
+		fmt::print("Connection attempt failed. Will try to reconnect.\n");
+		client_.reconnect();
+	}
+
+	void on_success(const mqtt::token& tok) override {
+		fmt::print("Succeed.\n");
+	}
+
+	void connected(const std::string& cause) override {
+		fmt::print("Connection success. Will subscribte to {}.\n", topic_);
+		client_.subscribe(topic_, qos_, nullptr, *this);
+	}
+
+	void connection_lost(const std::string& cause) override {
+		fmt::print("Connection lost: {}.\nReconnecting...\n", cause);
+		client_.reconnect();
+	}
+
+	void message_arrived(mqtt::const_message_ptr msg) override {
+		fmt::print("Topic: {} - Payload: {}\n", msg->get_topic(), msg->to_string());
+	}
+
+	void delivery_complete(mqtt::delivery_token_ptr token) override {}
+
+public:
+	MQTT_Subscriber(const std::string& address, const std::string& topic, int qos) :
+		address_(address),
+		topic_(topic),
+		qos_(qos),
+		client_(address_, ""), // Force random clientID
+		connOpts{}
+	{
+		connOpts.set_keep_alive_interval(20);
+		connOpts.set_clean_session(true);
+		client_.set_callback(*this);
+		client_.connect(connOpts);
+	}
+
+	~MQTT_Subscriber()
+	{
+		try {
+			client_.unsubscribe(topic_)->wait();
+			client_.stop_consuming();
+			client_.disconnect()->wait();
+		}
+		catch (const mqtt::exception & exc) {
+			fmt::print(exc.what());
+		}
+	};
+};
+
 int main(void)
 {
-	mqtt::connect_options connOpts;
-	connOpts.set_keep_alive_interval(20);
-	connOpts.set_clean_session(true);
+	MQTT_Subscriber sub("tcp://192.168.72.249:1883", "MQTT", 0);
 
-	// Pass empty string to clientID field to force random clientID
-	mqtt::async_client cli("tcp://192.168.72.249:1883", "");
-
-	try {
-		cli.connect(connOpts)->wait();
-		cli.start_consuming();
-		cli.subscribe("MQTT", 0)->wait();
-
-		while (true) {
-			auto msg = cli.consume_message();
-			if (!msg) break;
-			fmt::print("{}: {}\n", msg->get_topic(), msg->to_string());
-		}
-
-		cli.unsubscribe("MQTT")->wait();
-		cli.stop_consuming();
-		cli.disconnect()->wait();
-	}
-	catch (const mqtt::exception & exc) {
-		fmt::print(exc.what());
-		return 1;
-	}
+	std::getchar();
 
 	return 0;
 }
